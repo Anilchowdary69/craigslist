a99eec5c85102e9d3921393db4c879e82eeff392
Anilchowdary <anilpoka69@gmail.com>
2022-02-17 18:39:33 +0530

    commit

--- craigslist/urls.py
+++ craigslist/urls.py
@@ -15,10 +15,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-
-
-
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("",include('my_app.urls')),
