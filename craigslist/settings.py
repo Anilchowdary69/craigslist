9a83cbf642393b3691ae11305f3da583965aade4
Anilchowdary <anilpoka69@gmail.com>
2022-02-17 13:55:02 +0530

    second

--- craigslist/settings.py
+++ craigslist/settings.py
@@ -121,9 +121,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = str(BASE_DIR.joinpath('static')),
-django_heroku.settings(locals())
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+django_heroku.settings(locals())
