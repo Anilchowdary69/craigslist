c215243525413d78bbd70ee5ed21d772ca9440d3
Anilchowdary <anilpoka69@gmail.com>
2022-02-17 15:48:36 +0530

    third

--- craigslist/settings.py
+++ craigslist/settings.py
@@ -26,9 +26,9 @@ TEMPLATE_DIR=str(BASE_DIR.joinpath('templates'))
 SECRET_KEY = 'django-insecure-*gt@e!tgl0&_2g=8(x#y1p7fxng*z^lc9vdkkvj#5n353236ob'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [127.0.0.1]
 
 
 # Application definition
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
